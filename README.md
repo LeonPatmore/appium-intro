dc95bd973021d04246356e1a3f7c2b5e9f9d5a1d
LeonPatmore <leon.patmore@vonage.com>
2021-01-13 21:01:28 +0000

    Finding app which works.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Appium Intro
 
 Intended to be a simple introduction to Appium in Python, with some good examples.
+
+## Setting Android home environment variable.
+
+`set ANDROID_HOME=C:\Users\Leon\AppData\Local\Android\Sdk`
--- test.py
+++ test.py
@@ -8,9 +8,9 @@ PATH = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '4.2'
+desired_caps['platformVersion'] = '11.0'
 desired_caps['deviceName'] = 'Android Emulator'
-desired_caps['app'] = PATH('test-app.apk')
+desired_caps['app'] = PATH('test-app-2.apk')
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
